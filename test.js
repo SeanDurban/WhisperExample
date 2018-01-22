b18b1e51474197f855bca8068f26d27baec1d150
SeanDurban <seandurban10@gmail.com>
2018-01-22 23:35:10 +0000

    Update key gen with simple password

--- test.js
+++ test.js
@@ -7,13 +7,14 @@ var symKeyID = null;
 var signature = null;
 
 Promise.all([
-    web3.shh.newSymKey().then((id) => {identities.push(id);symKeyID=id;}),
+    web3.shh.generateSymKeyFromPassword('testingSymKey').then((id) => {symKeyID = id;}),
+  //  web3.shh.newSymKey().then((id) => {identities.push(id);}),
     web3.shh.newKeyPair().then((id) => {identities.push(id);signature=id;})
 ]).then(function() {
   console.log('Keys created');
   var ts ='0xffddaa11';
   web3.shh.subscribe('messages', {
-      symKeyID: identities[0],
+      symKeyID: symKeyID,
       topics: ['0xffddaa11', '0xafddaa11','0xbfddaa11']
   }).on('data', (res) => {
       console.log("Received: ", web3.utils.hexToAscii(res.payload));
@@ -21,11 +22,11 @@ Promise.all([
 }).then(function() {
   console.log('subscribed to messages');
     web3.shh.post({
-        symKeyID: identities[0], // encrypts using the sym key ID
+        symKeyID: symKeyID, // encrypts using the sym key ID
         sig: identities[1], // signs the message using the keyPair ID
         ttl: 20,
-        topic: '0xcfddaa11',
-        payload: web3.utils.asciiToHex('Hello World!'),
+        topic: '0xbfddaa11',
+        payload: web3.utils.asciiToHex('Hello World2!'),
         powTime: 3,
         powTarget: 0.5
     }, (err, res) => {
